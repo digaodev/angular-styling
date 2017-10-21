9ead357e963aec8dee48fbc8b61bd24c604c2ee1
digaodev <rodrigo.rb.dev@gmail.com>
2017-10-21 13:53:50 -0200

    minor fixes in animation and css

--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -15,7 +15,7 @@ export class ProjectsService {
     const prjLoader = Observable.create((observer: Observer<Project[]>) => {
       setTimeout(() => {
         observer.next(this.projects);
-      }, 2000);
+      }, 2500);
     });
     return prjLoader;
   }
